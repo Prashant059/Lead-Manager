28bf712b85ee4fdde1034cf6d4bca270514a9ccc
Prashant Randive <prashant.randhive05@gmail.com>
2025-09-07 18:45:07 +0530

    Setup GitHub Pages deployment with Vite and React Router

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Lead-Manager">
       <App />
     </BrowserRouter>
   </React.StrictMode>
